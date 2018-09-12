3aa319222b88e1061394ad3f5da106d23dc42516
tanyamatesha <tanya-matesha@rambler.ru>
2018-09-12 23:00:30 +0300

    sorter is complited.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,41 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.array = [];
+        this.comparator = (a, b) => a - b;
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.array.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.array[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.array.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.array;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        indices.sort();
+        let tempArray = [];
+        for (let i = 0; i < indices.length; i++) {
+            tempArray.push(this.array[indices[i]]);
+        }
+        tempArray.sort(this.comparator);
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+        for (let i = 0; i < tempArray.length; i++) {
+            this.array[indices[i]] = tempArray[i];
+        }
+    }
+
+    setComparator(compareFunction) {
+        this.comparator = compareFunction;
+    }
 }
 
 module.exports = Sorter;
